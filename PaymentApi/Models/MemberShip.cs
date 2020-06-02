5fb6926d587e316154cdd245dd0da6df234126a6
nandeeshs <nandeeshs@outlook.com>
2020-06-02 16:45:10 +0530

    Saving model class

---
+++ PaymentApi/Models/MemberShip.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PaymentApi.BL_CLASS
+{
+    /// <summary>
+    /// Accept request and process email
+    /// </summary>
+    /// <seealso cref="PaymentApi.BL_CLASS.IPayment" />
+    public class MemberShip : IPayment
+    {
+        public bool ProcessPayment(PaymentRequest objrequest, out string response)
+        {
+            try
+            {
+                var productType = (MemberShipType)objrequest.PaymentType;
+                switch (productType)
+                {
+                    case MemberShipType.Activate:
+                        response = SendEmail(objrequest);
+                        break;
+                    case MemberShipType.Deactivate:
+                        response = SendEmail(objrequest);
+                        break;
+                    case MemberShipType.Upgrade:
+                        response = SendEmail(objrequest);
+                        break;
+                    default:
+                        response = "Invalid Memebership type";
+                        return false;
+                }
+                return true;
+            }
+            catch (Exception er)
+            {
+                response = er.Message;
+                return false;
+            }
+        }
+
+        public string SendEmail(PaymentRequest objrequest)
+        {
+
+            return "Email sent sucessfully";
+
+        }
+    }
+}
---
+++ PaymentApi/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PaymentApi.BL_CLASS
+{
+    /// <summary>
+    /// Genrate Slip
+    /// </summary>
+    /// <seealso cref="PaymentApi.BL_CLASS.IPayment" />
+    public class Product : IPayment
+    {
+        public bool ProcessPayment(PaymentRequest objrequest,out string response)
+        {
+            try
+            {
+                var productType = (ProductType)objrequest.PaymentType;
+                switch (productType)
+                {
+                    case ProductType.PhysicalProduct :
+                        response = GenrateSlip(objrequest);
+                        break;
+                    case ProductType.Video :
+                        response = GenrateSlip(objrequest);
+                        break;
+                    case ProductType.Book:
+                        response = GenrateSlip(objrequest);
+                        break;
+                    default:
+                        response = "Invalid product type";
+                        return false;
+                }
+                return true;
+            }
+            catch (Exception er)
+            {
+                response = er.Message;
+                return false;
+            }
+        }
+
+        public string GenrateSlip(PaymentRequest objrequest)
+        {
+          
+            return "<html>Slip Genrated sucessfully</html>";
+                
+        }
+    }
+}
