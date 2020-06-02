293094bda16bfa0b82441f604500566b8bb2166a
nandeeshs <nandeeshs@outlook.com>
2020-06-02 16:40:03 +0530

    Commiting BL classes

---
+++ PaymentApi/BL_CLASS/FactoryClass.cs
@@ -0,0 +1,24 @@
+﻿namespace PaymentApi.BL_CLASS
+{
+    /// <summary>
+    /// Its a factory class: The creation of object is done here
+    /// </summary>
+    public class FactoryClass
+    {
+
+        public IPayment GetPaymentType(PaymentRequest objRequest)
+        {
+            IPayment objpaPayment = null;
+            switch (objRequest.PaymentCategory)
+            {
+                case PaymentCategory.Memebership:
+                    objpaPayment=new MemberShip();
+                    break;
+                case PaymentCategory.Product:
+                    objpaPayment=new Product();
+                    break;
+            }
+            return objpaPayment;
+        }
+    }
+}
---
+++ PaymentApi/BL_CLASS/PaymentRequest.cs
@@ -0,0 +1,74 @@
+﻿namespace PaymentApi.BL_CLASS
+{
+    /// <summary>
+    /// Payment request parameters
+    /// </summary>
+    public class PaymentRequest
+    {
+        //Payment id
+        public int PaymentId { get; set; }
+
+        //Price of product
+        public int Price { get; set; }
+
+        //Username of payment
+        public string UserName { get; set; }
+
+        //payment category
+        public PaymentCategory PaymentCategory { get; set; }
+
+        //Type of payment
+        public int PaymentType { get; set; }
+
+    }
+
+    /// <summary>
+    /// Response for payment requst
+    /// </summary>
+    public class PaymentResponse
+    {
+        //200=Sucess  
+        public int StatusCode { get; set; }
+        //resonse description
+        public string StatusDesc { get; set; }
+        // unique messageid for request
+        public string MessageId { get; set; }
+    }
+
+    /// <summary>
+    /// Payment Category
+    /// </summary>
+    public enum PaymentCategory
+    {
+        Product=1,
+        Memebership=2,
+    }
+
+    /// <summary>
+    /// Product Type
+    /// </summary>
+    public enum ProductType
+    {
+        PhysicalProduct=1,
+        Book=2,
+        Video=3,
+    }
+
+    /// <summary>
+    /// MemberShip Type
+    /// </summary>
+    public enum MemberShipType
+    {
+        Activate = 1,
+        Deactivate = 2,
+        Upgrade = 3,
+    }
+
+    /// <summary>
+    /// Used to decouple client class  and business class
+    /// </summary>
+    public interface IPayment
+    {
+        bool ProcessPayment(PaymentRequest objrequest, out string response);
+    }
+}
